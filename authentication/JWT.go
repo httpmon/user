a614a0409e5ba6c1514f634df097f53ab61d8bfc
raha <elahe.dstn@gmail.com>
2020-07-24 17:10:03 +0430

    Add JWT authentication

---
+++ authentication/JWT.go
@@ -0,0 +1,47 @@
+package authentication
+
+import (
+	"time"
+	"user/config"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func CreateToken(id int, cfg config.JWT) (string, error) {
+	var err error
+
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = id
+	atClaims["exp"] = time.Now().Add(time.Minute * time.Duration(cfg.Expiration)).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(cfg.Secret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+//nolint: gofumpt
+func ValidateToken(token string, cfg config.JWT) (in bool, i int) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(cfg.Secret), nil
+	})
+
+	if err != nil {
+		return false, 0
+	}
+
+	auth := claims["authorized"].(bool)
+	exp := claims["exp"].(float64)
+	id := claims["user_id"].(float64)
+
+	if auth && exp > float64(time.Now().Unix()) {
+		return true, int(id)
+	}
+
+	return false, int(id)
+}
