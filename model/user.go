25a819210f035fbdb11e3ef8dfeb19688eb0bf76
raha <elahe.dstn@gmail.com>
2020-07-25 14:27:33 +0430

    Add status to model

--- model/user.go
+++ model/user.go
@@ -1,8 +1,8 @@
 package model
 
 type User struct {
-	ID       int    `gorm:"primaryKey;AUTO_INCREMENT"`
-	Email    string `gorm:"unique;not null"`
-	Password string `gorm:"not null"`
-	Urls     []URL  `gorm:"foreignkey:UserID"`
+	ID       int
+	Email    string
+	Password string
+	Urls     []URL
 }
