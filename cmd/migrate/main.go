5e15a65a81c01c6b8a15482800c837ebe4b8b0f5
raha <elahe.dstn@gmail.com>
2020-07-29 13:56:01 +0430

    Add forgotten driver

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/source/file" //Imported for its side effects
 	"github.com/spf13/cobra"
 )
 
